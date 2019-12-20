5baa397478d4e4d76a89d0b17e8875daab85d0c2
Ali Sawahreh <sawahreh-ali@hotmail.com>
2019-12-20 21:48:14 +0200

    Get the values from the form

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var addNoteBtn = document.getElementById('addNoteBtn');
+
+addNoteBtn.addEventListener('click', function() {
+    let task = document.getElementById('task').value;
+    let deadline = document.getElementById('deadline').value;
+    console.log(task);
+    console.log(deadline);
+
+});
