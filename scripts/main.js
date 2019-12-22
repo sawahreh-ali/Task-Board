3757c740f67027a043404fe6da7e33edd5f44e09
Ali Sawahreh <sawahreh-ali@hotmail.com>
2019-12-22 21:39:49 +0200

    add note

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,21 @@
+var Note = function(task, deadline) {
+    this.task = task;
+    this.deadline = deadline;
+};
+var id = 0;
 var addNoteBtn = document.getElementById('addNoteBtn');
-
-addNoteBtn.addEventListener('click', function() {
+function addNote(){
     let task = document.getElementById('task').value;
-    let deadline = document.getElementById('deadline').value;
-    console.log(task);
-    console.log(deadline);
-
-});
+    let deadline = new Date(document.getElementById('deadline').value).getTime();
+    if(deadline > Date.now()){
+        var note = new Note(task, deadline);
+        var main = document.querySelector('main');
+        main.innerHTML += `<div class='notes py-4 px-1' id='note-${id++}'>
+                            <p>${task}</p>
+                            <span>${new Date(deadline).toLocaleDateString()}</span>
+                        <div>`;
+    }
+    else {
+        /* Validate deadline here*/ 
+    } 
+}
