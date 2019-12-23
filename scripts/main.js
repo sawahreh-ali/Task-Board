f682b6d42facc9f559d46c400dcee3b4aff9917d
Ali Sawahreh <sawahreh-ali@hotmail.com>
2019-12-23 10:48:13 +0200

    add note with date and time

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,24 @@ var Note = function(task, deadline) {
     this.deadline = deadline;
 };
 var id = 0;
+var notes = [];
 var addNoteBtn = document.getElementById('addNoteBtn');
 function addNote(){
     let task = document.getElementById('task').value;
-    let deadline = new Date(document.getElementById('deadline').value).getTime();
-    if(deadline > Date.now()){
-        var note = new Note(task, deadline);
+    let deadlineDate = new Date(document.getElementById('deadlineDate').value).toLocaleDateString();
+    let deadlineTime = document.getElementById('deadlineTime').value;
+    var dateTime = new Date(`${deadlineDate} ${(deadlineTime ? deadlineTime: '11:59')}`).getTime();
+    if( dateTime > Date.now()){
+        notes.push(new Note(task, dateTime));
         var main = document.querySelector('main');
         main.innerHTML += `<div class='notes py-4 px-1' id='note-${id++}'>
                             <p>${task}</p>
-                            <span>${new Date(deadline).toLocaleDateString()}</span>
+                            <span>${new Date(deadlineDate).toLocaleDateString()}</span> <br />
+                            <span>${deadlineTime}</span>
                         <div>`;
     }
     else {
         /* Validate deadline here*/ 
     } 
+    console.log(notes);
 }
