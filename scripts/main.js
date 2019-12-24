a3820b51949f14bac0741ff4eafd2a4d4b859229
Ali Sawahreh <sawahreh-ali@hotmail.com>
2019-12-24 18:00:54 +0200

    retrive data from localstorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-var Note = function(task, deadline, id) {
+var Note = function(task, date, time, id) {
     this.task = task;
-    this.deadline = deadline;
+    this.date = date;
+    this.time = time;
     this.id = id;
 };
 var id = 0;
@@ -8,18 +9,18 @@ var notes = [];
 var addNoteBtn = document.getElementById('addNoteBtn');
 function addNote(){
     let task = document.getElementById('task').value;
-    let deadlineDate = new Date(document.getElementById('deadlineDate').value).toLocaleDateString();
-    let deadlineTime = document.getElementById('deadlineTime').value;
-    var dateTime = new Date(`${deadlineDate} ${(deadlineTime ? deadlineTime: '23:59')}`).getTime();
+    let date = new Date(document.getElementById('deadlineDate').value).toLocaleDateString();
+    let time = document.getElementById('deadlineTime').value;
+    var dateTime = new Date(`${date} ${(time ? time: '23:59')}`).getTime();
     if( dateTime > Date.now()){
-        let note = new Note(task, dateTime, id);
+        let note = new Note(task, date, time, id);
         notes.push(note);
         var main = document.querySelector('main');
         main.innerHTML += ` <div class='notes py-4 px-1' id='note-${id}'>
                                 <i class="fas fa-times mr-4" onclick="deleteNote(${id})"></i>
                                 <p>${task}</p>
-                                <span>${new Date(deadlineDate).toLocaleDateString()}</span> <br />
-                                <span>${deadlineTime}</span>
+                                <span>${date}</span> <br />
+                                <span>${time}</span>
                             <div>`;
         localStorage.setItem(id++, JSON.stringify(note));
     }
@@ -35,3 +36,18 @@ function deleteNote(id) {
     localStorage.removeItem(id);
     document.getElementById(`note-${id}`).remove();
 }
+
+window.onload = function() {
+    if(window.localStorage.length){
+        var main = document.querySelector('main');
+        for(let i = 0; i < window.localStorage.length; i++){
+            var note = JSON.parse(this.localStorage[i]);
+            main.innerHTML += ` <div class='notes py-4 px-1' id='note-${note.id}'>
+                                    <i class="fas fa-times mr-4" onclick="deleteNote(${note.id})"></i>
+                                    <p>${note.task}</p>
+                                    <span>${note.date}</span> <br />
+                                    <span>${note.time}</span>
+                                <div>`;
+        }
+    }
+};
